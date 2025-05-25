b73741a73332aa63d8326559585272419335e69e
Birras <119614845+BirrasOBacano@users.noreply.github.com>
2025-05-25 16:29:47 +0100

    remove unused import

--- backend/src/main/java/tqs/msev/backend/controller/ReservationController.java
+++ backend/src/main/java/tqs/msev/backend/controller/ReservationController.java
@@ -1,6 +1,5 @@
 package tqs.msev.backend.controller;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
