00ad37ee333e39b02407baf4f4a18fe6f93775a1
Francisca Silva <franciscafpsilva@ua.pt>
2025-06-06 00:14:03 +0100

    Remove unused import

--- backend/src/test/java/tqs/msev/backend/controller/ChargerControllerTest.java
+++ backend/src/test/java/tqs/msev/backend/controller/ChargerControllerTest.java
@@ -26,7 +26,6 @@ import static org.springframework.security.test.web.servlet.request.SecurityMock
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
